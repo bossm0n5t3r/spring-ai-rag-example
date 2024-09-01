1e50e49bff9f6f7b031a42447d5d9cfd687d5e75
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-09-02 00:20:50 +0900

    feat: Add AskService

--- src/main/kotlin/me/bossm0n5t3r/rag/AskController.kt
+++ src/main/kotlin/me/bossm0n5t3r/rag/AskController.kt
@@ -1,12 +1,5 @@
 package me.bossm0n5t3r.rag
 
-import org.springframework.ai.chat.client.ChatClient
-import org.springframework.ai.chat.client.ChatClient.UserSpec
-import org.springframework.ai.document.Document
-import org.springframework.ai.vectorstore.SearchRequest
-import org.springframework.ai.vectorstore.VectorStore
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.io.Resource
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,35 +8,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/ask")
 class AskController(
-    private val aiClient: ChatClient,
-    private val vectorStore: VectorStore,
-    @Value("classpath:/rag-prompt-template.st")
-    private val ragPromptTemplate: Resource,
+    private val askService: AskService,
 ) {
     @PostMapping
     fun ask(
         @RequestBody question: Question,
-    ): Answer {
-        val similarDocuments: List<Document> =
-            vectorStore
-                .similaritySearch(
-                    SearchRequest
-                        .query(question.question)
-                        .withTopK(2),
-                )
-        val contentList = similarDocuments.map { it.content }
-
-        val answer =
-            aiClient
-                .prompt()
-                .user { userSpec: UserSpec ->
-                    userSpec
-                        .text(ragPromptTemplate)
-                        .param("input", question.question)
-                        .param("documents", java.lang.String.join("\n", contentList))
-                }.call()
-                .content()
-
-        return Answer(answer)
-    }
+    ): Answer = askService.ask(question)
 }
---
+++ src/main/kotlin/me/bossm0n5t3r/rag/AskService.kt
@@ -0,0 +1,42 @@
+package me.bossm0n5t3r.rag
+
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.ChatClient.UserSpec
+import org.springframework.ai.document.Document
+import org.springframework.ai.vectorstore.SearchRequest
+import org.springframework.ai.vectorstore.VectorStore
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
+import org.springframework.stereotype.Service
+
+@Service
+class AskService(
+    private val aiClient: ChatClient,
+    private val vectorStore: VectorStore,
+    @Value("classpath:/rag-prompt-template.st")
+    private val ragPromptTemplate: Resource,
+) {
+    fun ask(question: Question): Answer {
+        val similarDocuments: List<Document> =
+            vectorStore
+                .similaritySearch(
+                    SearchRequest
+                        .query(question.question)
+                        .withTopK(2),
+                )
+        val contentList = similarDocuments.map { it.content }
+
+        val answer =
+            aiClient
+                .prompt()
+                .user { userSpec: UserSpec ->
+                    userSpec
+                        .text(ragPromptTemplate)
+                        .param("input", question.question)
+                        .param("documents", contentList.joinToString("\n"))
+                }.call()
+                .content()
+
+        return Answer(answer)
+    }
+}
